03bc9b94977422674707ed429ad180296815a1ca
DEBJIT PUROHIT <92798917+debjitpurohit@users.noreply.github.com>
2024-02-08 12:12:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.post("/api/create-checkout-session",async(req,res)=>{
         payment_method_types:["card"],
         line_items:lineItems,
         mode:"payment",
-        success_url:"http://localhost:5173/sucess",
-        cancel_url:"http://localhost:5173/cancel",
+        success_url:"https://agri-frontend-user-3qhnb6eqf-debjitpurohit.vercel.app/sucess",
+        cancel_url:"https://agri-frontend-user-3qhnb6eqf-debjitpurohit.vercel.app/cancel",
     });
 
     res.json({id:session.id})
